947a6c5421d05c88826071066a35f0c9bf1bd546
Aaron Kollasch <aaron@kollasch.dev>
2025-12-09 19:45:20 -0500

    Add day07b solution

--- src/days/day07b.rs
+++ src/days/day07b.rs
@@ -1,52 +1,83 @@
+const NUM_COLUMNS: usize = 141;
+
 pub fn get_result(input: &[u8]) -> usize {
     let mut lines = input
         .split(|&b| b == b'\n')
         .filter(|&l| !l.is_empty())
         .step_by(2);
     let first_line = lines.next().unwrap();
-    let first_pos = first_line.iter().position(|&b| b == b'S').unwrap();
-    let mut state = Vec::<usize>::with_capacity(first_line.len());
+    let num_columns = first_line.len();
+    let first_pos = num_columns / 2;
+    #[cfg(debug_assertions)]
+    {
+        assert_eq!(
+            first_pos,
+            first_line.iter().position(|&b| b == b'S').unwrap()
+        );
+        println!("start pos: {}", first_pos);
+    }
+    let mut state = [0usize; NUM_COLUMNS];
     let mut new_state = state.clone();
-    state.push(first_pos);
-    let mut split_count = 0;
-    for line in lines {
+    state[first_pos] = 1;
+    for (i, line) in lines.enumerate() {
         #[cfg(debug_assertions)]
-        println!("{:?}", state);
-        state.iter().for_each(|&pos| {
+        println!(
+            "{}",
+            state
+                .iter()
+                .map(|&c| match c {
+                    _ if c > 1000000000000 => '█',
+                    _ if c > 10000000000 => '▉',
+                    _ if c > 1000000000 => '▊',
+                    _ if c > 10000000 => '▋',
+                    _ if c > 100000 => '▌',
+                    _ if c > 1000 => '▍',
+                    _ if c > 10 => '▎',
+                    _ if c > 0 => '▏',
+                    _ => ' ',
+                })
+                .collect::<String>()
+        );
+        (first_pos - i..=first_pos + i).for_each(|pos| {
+            if state[pos] == 0 {
+                return;
+            }
             match line[pos] {
                 b'^' => {
-                    split_count += 1;
-                    match new_state.last() {
-                        Some(&new_pos) if new_pos == pos - 1 => new_state.push(pos + 1),
-                        _ => {
-                            new_state.push(pos - 1);
-                            new_state.push(pos + 1);
-                        }
-                    };
+                    new_state[pos - 1] += state[pos];
+                    new_state[pos + 1] += state[pos];
                 }
                 _ => {
-                    match new_state.last() {
-                        Some(&new_pos) if new_pos == pos => {},
-                        _ => {
-                            new_state.push(pos);
-                        }
-                    };
+                    new_state[pos] += state[pos];
                 }
             };
+            state[pos] = 0;
         });
         (state, new_state) = (new_state, state);
-        new_state.clear();
     }
     #[cfg(debug_assertions)]
-    println!("{:?}", state);
-    split_count
+    println!(
+        "{}",
+        state
+            .iter()
+            .map(|&c| match c {
+                _ if c > 1000000000000 => '█',
+                _ if c > 10000000000 => '▉',
+                _ if c > 1000000000 => '▊',
+                _ if c > 10000000 => '▋',
+                _ if c > 100000 => '▌',
+                _ if c > 1000 => '▍',
+                _ if c > 10 => '▎',
+                _ if c > 0 => '▏',
+                _ => ' ',
+            })
+            .collect::<String>()
+    );
+    state.into_iter().sum()
 }
 
 pub fn main() {
-    print!(
-        "{} ",
-        get_result(include_bytes!("../../inputs/day07.txt"))
-    );
+    print!("{} ", get_result(include_bytes!("../../inputs/day07.txt")));
 }
 
 #[cfg(test)]
@@ -56,12 +87,12 @@ mod tests {
     #[test]
     fn correct_example_result() {
         let result = get_result(include_bytes!("../../inputs/day07.example.txt"));
-        assert_eq!(result, 21);
+        assert_eq!(result, 40);
     }
 
     #[test]
     fn correct_result() {
         let result = get_result(include_bytes!("../../inputs/day07.txt"));
-        assert_eq!(result, 1615);
+        assert_eq!(result, 43560947406326);
     }
 }
